f1018f89252017e6dfbb922a141eff387659c6cb
chan12-19 <wenshanchan@gmail.com>
2024-01-09 20:58:50 +0800

    AGAIN

---
+++ Accreditation/Models/Fileupload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Accreditation.Models;
+
+public partial class Fileupload
+{
+    public string FileName { get; set; } = null!;
+
+    public string CourseId { get; set; } = null!;
+
+    public byte[] Attachment { get; set; } = null!;
+
+    public string Syncoperation { get; set; } = null!;
+
+    public DateTime Syncversion { get; set; }
+}
